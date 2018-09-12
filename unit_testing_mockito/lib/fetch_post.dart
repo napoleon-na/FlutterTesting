91088b99974b04295c7632e11b96b815de2e0dda
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-09-12 19:39:20 +0900

    Mock dependencies using Mockito - 2. Create a function to test

---
+++ unit_testing_mockito/lib/fetch_post.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost(http.Client client) async {
+  final response =
+      await client.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
