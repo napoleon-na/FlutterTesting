e7fc584686305f1566df537c15266c7fef95dbdb
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-09-12 19:40:21 +0900

    Mock dependencies using Mockito - 3. Create a test file with a mock http.Client

---
+++ unit_testing_mockito/test/fetch_post_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:unit_testing_mockito/fetch_post.dart';
+import 'package:http/http.dart' as http;
+
+class MockClient extends Mock implements http.Client {}
+
+main() {
+}
