cd921d2c84bf5da842af9a416bdc0d7bd7b7afd3
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-09-19 17:36:49 +0900

    Introduction to Widget testing - 5. Search for our Widget using a Finder

--- widget_testing/lib/main.dart
+++ widget_testing/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
     await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
+
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
   });
 }
 
