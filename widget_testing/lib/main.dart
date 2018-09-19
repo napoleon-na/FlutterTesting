395cf29731f7a247e6071a93430e8fa36ab4120c
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-09-19 17:43:19 +0900

    Introduction to Widget testing - 6. Verify our Widget is working using a Matcher

--- widget_testing/lib/main.dart
+++ widget_testing/lib/main.dart
@@ -7,6 +7,9 @@ void main() {
 
     final titleFinder = find.text('T');
     final messageFinder = find.text('M');
+
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
   });
 }
 
