e7a12bdb05ce570f6ea0ad34df62756bce5bd077
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-09-20 19:44:24 +0900

    Finding Widgets in a Widget Test - 1. Find a Text Widget

--- widget_test_finders/test/widget_test.dart
+++ widget_test_finders/test/widget_test.dart
@@ -7,23 +7,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:widget_test_finders/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+  testWidgets('finds a Text Widget', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Text('H'),
+      ),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('H'), findsOneWidget);
   });
 }
